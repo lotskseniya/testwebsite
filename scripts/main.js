184b7fd5ee8d461ef1bcc5ab8f0a0ac30dd2de41
lotskseniya <kseniyalotsman@gmail.com>
2022-05-03 14:13:22 +0300

    added project files

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+sign = document.getElementById("signin")
+popup = document.getElementById("popup")
+sign.addEventListener('click',function (){
+    popup.classList.add("active_popup")
+})
+var close = document.querySelector(".popup_close_btn")
+close.addEventListener('click', function (){
+    popup.classList.remove("active_popup")
+});
+
+$('.autoplay').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    speed: 500,
+    slide: 'div',
+  }); 
+
+
+
+ 
+
